e223e117212166fff4df13c19899036683a4caf8
Alex Hall <halla22@my.erau.edu>
2020-07-17 03:27:10 -0400

    Added routes and served the /public directory

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const morgan = require('morgan');
 const clientSession = require('client-sessions');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 
 const {SESSION_SECRET} = require('./config');
 
@@ -12,18 +13,20 @@ const api = require('./src/api');
 // routes
 var homeRouter = require('./routes/home');
 var mappingRouter = require('./routes/mapping');
+var parcelsRouter = require('./routes/parcels');
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-
-app.use('/', homeRouter);
-app.use('/mapping', mappingRouter);
-app.get('/health', (request, response) => response.sendStatus(200));
+// Setting up the middleware
 
 app.use(morgan('short'));
 app.use(express.json());
+app.use(bodyParser.urlencoded(
+  { extended: true }
+));
+app.use(bodyParser.json());
 app.use(
   clientSession({
     cookieName: 'session',
@@ -32,8 +35,19 @@ app.use(
   })
 );
 app.use(helmet());
-
 app.use(api);
+
+//  __dirname tells you the absolute path of the directory 
+//  containing the currently executing file
+app.use('/public', express.static(path.join(__dirname, '/public')));
+console.log(__dirname)
+
+// add route-handling code
+
+app.use('/', homeRouter);
+app.use('/mapping', mappingRouter);
+app.use('/parcels', parcelsRouter);
+app.get('/health', (request, response) => response.sendStatus(200));
 
 let server;
 module.exports = {
