0559f58a1815e2097316ef5367aa2f5f2bdceb08
Alex Hall <halla22@my.erau.edu>
2020-07-19 22:11:39 -0400

    insertDataInGeoJson() plots avg household income

--- public/js/loadMap.js
+++ public/js/loadMap.js
@@ -14,7 +14,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibGV0aGFsY2FmZmVpbmUiLCJhIjoiY2tiemlicjByMWEyeTJ2bnQxaDJoMnUzayJ9.YE5cT09nbl3jCO45Oh8Qog'
 }).addTo(mymap);
 
-parcelLayer = L.geoJSON().addTo(mymap);
+parcelLayer = L.geoJSON(false, {style: function(feature) {
+    switch (true) {
+        case (feature.properties.INCOME > 50000):
+            return {color: "#ff0000"};
+        case (feature.properties.INCOME < 50000):
+            return {color: "#0000ff"};
+    }}}).addTo(mymap);
 
 function makeAjaxGetCall(url) {
     return $.ajax({
@@ -87,79 +93,119 @@ $('#plotBlockGroups').on('click', function(event){
 
     function getGeoJson() {
         let url = "https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/Tracts_Blocks/MapServer/8/query?where=STATE%3D+%2712%27+AND+COUNTY+%3D+%27127%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=geojson"
-
         var response = fetch(url).then(response => {
             return response.json();
         });
-
-        response.then(data => {
-            data.features.sort((a, b) => parseInt(a.properties.BLKGRP) - parseInt(b.properties.BLKGRP));
-            console.log("sorted");
-            data.features.forEach(item => {
-                console.log(item.properties.BLKGRP + " " + item.properties.TRACT);
-            });
-        });
+        return response;
     }
 
     function getBlockGroupData() {
         var key = '125d1b7224cadc62c574b7c23b49e700d740bbb4';
-        var getParams='NAME,B19001_001E';
+        var getParams='NAME,B19001_001E,B19025_001E';
         let url = `https://api.census.gov/data/2018/acs/acs5?get=${getParams}&for=block%20group:*&in=state:12%20county:127%20tract:*&key=${key}`;
-
         var response = fetch(url).then(response => {
                 return response.json();
             });
-        
-        response.then(data => {
-            var dict = {};
-            data[0].forEach((item, index) => {
-                console.log(item);
-                dict[item] = index;
-            });
-
-            data.sort((a, b) => parseInt(a[dict["block group"]]) - parseInt(b[dict["block group"]]));
-            data.forEach(row => {
-                console.log(row[dict["block group"]] + " " + row[dict["tract"]]);
-            }); 
-        });
+        return response;
     }
+
+//      promise chain version of this function is not working
+//     function insertDataInGeoJson() {
+
+//         getGeoJson()
+//             .then(geojson => {
+//                 geojson.features.sort((a, b) => parseInt(a.properties.TRACT) - parseInt(b.properties.TRACT));
+//                 const blockData = getBlockGroupData();
+//                 return Promise.all([geojson, blockData]);
+//             })
+//             .then(([geojson, blockData]) => {
+//                 var dict = {};
+//                 blockData[0].forEach((item, index) => {
+//                     dict[item] = index;
+//                 });
+//                 blockData.sort((a, b) => parseInt(a[dict["tract"]]) - parseInt(b[dict["tract"]]));
+//                 for (var i=1; i<blockData.length; ++i) {
+
+//                     try {
+//                         //There can be duplicate tracts, so we filter on the common tract numbers and then on block group to get the correct geometry.
+//                         var matchingTract = blockData.filter(geom => geom[dict["tract"]] == geojson.features[i].properties.TRACT);
+//                         var matchingBlockGroup = matchingTract.filter(geom => geom[dict["block group"]] == geojson.features[i].properties.BLKGRP);
+//                         geojson.features[i].properties.INCOME = matchingBlockGroup[0][dict["B19001_001E"]]
+//                     }
+//                     catch (e) {
+//                         continue;
+//                     }
+//                 }
+//                 return Promise.all(geojson);
+//             });
+//     }
+//     insertDataInGeoJson().then(geojson => {parcelLayer.addData(geojson)});
+// });
+
+
+
 
     function insertDataInGeoJson() {
 
-        getGeoJson().then(data => {
-            data.features.sort((a, b) => parseInt(a.properties.BLKGRP) - parseInt(b.properties.BLKGRP));
+        getGeoJson().then(geojson => {
+            geojson.features.sort((a, b) => parseInt(a.properties.TRACT) - parseInt(b.properties.TRACT));
+
             getBlockGroupData().then(blockData => {
                 var dict = {};
                 blockData[0].forEach((item, index) => {
                     dict[item] = index;
                 });
-                blockData.sort((a, b) => parseInt(a[dict["block group"]]) - parseInt(b[dict["block group"]]));
-                data.features.forEach(item => {
-                    item.properties.INCOME = blockData[dict["B19001_001E"]];
-                    console.log(item.properties.INCOME);
-                });
+                blockData.sort((a, b) => parseInt(a[dict["tract"]]) - parseInt(b[dict["tract"]]));
+
+                for (var i=1; i<blockData.length; ++i) {
+
+                    try {
+                        //There can be duplicate tracts, so we filter on the common tract numbers and then on block group to get the correct geometry.
+                        var matchingTract = blockData.filter(geom => geom[dict["tract"]] == geojson.features[i].properties.TRACT);
+                        var matchingBlockGroup = matchingTract.filter(geom => geom[dict["block group"]] == geojson.features[i].properties.BLKGRP);
+                        geojson.features[i].properties.INCOME = matchingBlockGroup[0][dict["B19025_001E"]] / matchingBlockGroup[0][dict["B19001_001E"]];
+                        console.log(geojson.features[i].properties.INCOME);
+                    }
+                    catch (e) {
+                        continue;
+                    }
+                }
+
+                // L.geoJSON(geojson, {
+                //     style: function(feature) {
+                //         switch (true) {
+                //             case (feature.properties.INCOME > 700):
+                //                 return {color: "#ff0000"};
+                //             case (feature.properties.INCOME < 700):
+                //                 return {color: "#0000ff"};
+                //         }
+                //     }
+                // }).addTo(mymap);
+
+                // parcelLayer.addData(geojson, {
+                //     style: function(feature) {
+                //         switch (true) {
+                //             case (feature.properties.INCOME > 700):
+                //                 return {color: "#ff0000"};
+                //             case (feature.properties.INCOME < 700):
+                //                 return {color: "#0000ff"};
+                //         }
+                //     }
+                // });
+
+                parcelLayer.addData(geojson);
+
             }); 
         });
     }
 
-    getGeoJson();
-    getBlockGroupData();
-
-    function applyStyle(){
-
-        var incomeStyle = {
-            "color": "#ff7800",
-            "weight": 5,
-            "opacity": 0.65
-        };
-
-    }
+    insertDataInGeoJson();
 
 });
 
-$.getJSON("public/geojson/blockgroups.geojson", function(data) {
-    addFeatureToLayer(data);
-}).fail(function(){
-    console.log("Could not get inside $.getJSON");
-});
+// $.getJSON("public/geojson/blockgroups.geojson", function(data) {
+//     addFeatureToLayer(data);
+// }).fail(function(){
+//     console.log("Could not get inside $.getJSON");
+// });
     
